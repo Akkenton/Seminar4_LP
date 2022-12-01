a8b684033e47b60417187ead9b99d262a50b361a
Akkenton <akkenton.msk@gmail.com>
2022-12-01 20:30:05 +0300

    Решение задачи арифметической прогресси

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SummA(int count)
+{
+    int result = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int valueA = int.Parse(Console.ReadLine());
+
+int result = SummA(valueA);
+Console.WriteLine($"Итоговое значение: {result}");
