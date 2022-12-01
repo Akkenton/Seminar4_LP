11884504197c98541010057044e9545d656314d2
Akkenton <akkenton.msk@gmail.com>
2022-12-01 22:00:50 +0300

    Решение задачи по заполнению массива 0 и 1 рандомом

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int LengthOfArray()
+{
+    Console.Clear();
+    Console.WriteLine("Введите длинну масива: ");
+    int length = int.Parse(Console.ReadLine());
+    return length;
+}
+
+int[] FillArrayRandom(int size)
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i]);
+    }
+}
+
+int size = LengthOfArray();
+int[] arr = FillArrayRandom(size);
+PrintArray(arr, size);
