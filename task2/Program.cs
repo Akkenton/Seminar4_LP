63e92ab244d38fefaef9e0a93eb9774c3963ec9e
Akkenton <akkenton.msk@gmail.com>
2022-12-01 20:53:57 +0300

    Решение задачи №1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+// ---------------------------------- START  ---------------------------------------------------------------
+
+int DataEntry() // метод ввода числа
+{
+    Console.Clear();
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+
+int CountOfNumber(int number)  // метод подсчета количества чисел
+{
+    int count = 0;
+    if (number < 0) number = -number;
+    for (int i = 0; number > 0; i++)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+// ---------------------------------------  Рабочая область  ------------------------------------------------
+
+int number = DataEntry();
+int count = CountOfNumber(number);
+
+Console.WriteLine($"Количество цифр: {count}");
+
+// ---------------------------------------  END  ----------------------------------------------------------
