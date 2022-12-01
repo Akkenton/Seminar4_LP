73de803634aba6d5e1486dd70285f16b5601f1c2
Akkenton <akkenton.msk@gmail.com>
2022-12-01 21:26:54 +0300

    Решение задачи про умножение до числа N

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int NumberAdd()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int ResultNumber(int number)
+{
+    int result = 1;
+    for (var i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int value = NumberAdd();
+int result = ResultNumber(value);
+Console.WriteLine($"Значение: {result}");
